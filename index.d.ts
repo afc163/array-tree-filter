c5d512b74753d9b3b5292efa2752a029694a87fd
afc163 <afc163@gmail.com>
2024-10-22 13:31:30 +0800

    Delete index.d.ts

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const arrayTreeFilter: <T>(data: T[], filterFn: (item: T, level: number) => boolean, options?: {
-    childrenKeyName?: string | undefined;
-} | undefined) => T[];
-export default arrayTreeFilter;
